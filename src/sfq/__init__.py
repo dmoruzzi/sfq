003b758486912d643bdb347e57d55a28af492f02
David Moruzzi <dmoruzzi@users.noreply.github.com>
2024-12-14 07:26:39 -0500

    Proxy Support & Bugfixes

--- src/sfq/__init__.py
+++ src/sfq/__init__.py
@@ -1,6 +1,7 @@
 import http.client
 import logging
 import time
+import os
 import json
 from urllib.parse import urlparse, quote
 
@@ -17,6 +18,7 @@ class SFAuth:
         access_token=None,
         token_expiration_time=None,
         token_lifetime=15 * 60,
+        proxy="auto",
     ):
         """
         Initializes the SFAuth with necessary parameters.
@@ -29,6 +31,7 @@ class SFAuth:
         :param access_token: The access token for the current session (default is None).
         :param token_expiration_time: The expiration time of the access token (default is None).
         :param token_lifetime: The lifetime of the access token (default is 15 minutes).
+        :param proxy: The proxy configuration (default is "auto").
         """
         self.instance_url = instance_url
         self.client_id = client_id
@@ -38,6 +41,19 @@ class SFAuth:
         self.access_token = access_token
         self.token_expiration_time = token_expiration_time
         self.token_lifetime = token_lifetime
+        self._auto_configure_proxy(proxy)
+
+    def _auto_configure_proxy(self, proxy):
+        """
+        Automatically configure the proxy based on the environment.
+        """
+        if proxy == "auto":
+            if "https_proxy" in os.environ:
+                self.proxy = os.environ["https_proxy"]
+            else:
+                self.proxy = None
+        else:
+            self.proxy = proxy
 
     def _prepare_payload(self):
         """Prepare the payload for the token request."""
@@ -47,13 +63,28 @@ class SFAuth:
             "refresh_token": self.refresh_token,
         }
 
+    def _create_connection(self, netloc):
+        """
+        Create a connection using HTTP or HTTPS, depending on the proxy configuration.
+        """
+        if self.proxy:
+            proxy_url = urlparse(self.proxy)
+            if proxy_url.scheme == "http://":
+                conn = http.client.HTTPConnection(proxy_url.hostname, proxy_url.port)
+            else:
+                conn = http.client.HTTPSConnection(proxy_url.hostname, proxy_url.port)
+            conn.set_tunnel(netloc)
+        else:
+            conn = http.client.HTTPSConnection(netloc)
+        return conn
+
     def _send_post_request(self, payload):
         """Send a POST request to the Salesforce token endpoint using http.client."""
         parsed_url = urlparse(self.instance_url)
-        conn = http.client.HTTPSConnection(parsed_url.netloc)
+        conn = self._create_connection(parsed_url.netloc)
 
         headers = {"Content-Type": "application/x-www-form-urlencoded"}
-        body = "&".join([f"{key}={value}" for key, value in payload.items()])
+        body = "&".join([f"{key}={quote(str(value))}" for key, value in payload.items()])
 
         try:
             conn.request("POST", self.token_endpoint, body, headers)
@@ -75,7 +106,7 @@ class SFAuth:
 
     def _refresh_token_if_needed(self):
         """Automatically refresh the token if it has expired or is missing."""
-        _token_expiration = self._is_token_expired
+        _token_expiration = self._is_token_expired()
         if self.access_token and not _token_expiration:
             return
         
@@ -118,7 +149,7 @@ class SFAuth:
         params = f"?q={encoded_query}"
 
         parsed_url = urlparse(self.instance_url)
-        conn = http.client.HTTPSConnection(parsed_url.netloc)
+        conn = self._create_connection(parsed_url.netloc)
 
         try:
             conn.request("GET", query_endpoint + params, headers=headers)
