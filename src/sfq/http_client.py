29c5374c6d0709d8995673af8c1d08d1e7881f52
David Moruzzi <dmoruzzi@users.noreply.github.com>
2025-08-24 01:50:04 -0400

    chore: fix bad test case

--- src/sfq/http_client.py
+++ src/sfq/http_client.py
@@ -28,7 +28,7 @@ class HTTPClient:
     def __init__(
         self,
         auth_manager: AuthManager,
-        user_agent: str = "sfq/0.0.36",
+        user_agent: str = "sfq/0.0.37",
         sforce_client: str = "_auto",
         high_api_usage_threshold: int = 80,
     ) -> None:
